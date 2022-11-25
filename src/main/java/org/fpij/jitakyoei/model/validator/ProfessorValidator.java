82ae6b8556b2998f31a3b9e524a36fd99e2322e9
Thiago Soares <13428676+ThiagoS332@users.noreply.github.com>
2022-11-24 23:21:34 -0300

    FaixaValidator
    Added the validator for Faixas and implemented the validation in Aluno and Professor Validators.

--- src/main/java/org/fpij/jitakyoei/model/validator/ProfessorValidator.java
+++ src/main/java/org/fpij/jitakyoei/model/validator/ProfessorValidator.java
@@ -7,6 +7,7 @@ public class ProfessorValidator implements Validator<Professor> {
     private RgValidator rgVal = new RgValidator();
 	private EnderecoValidator enderecoVal = new EnderecoValidator();
 	private EntidadeValidator entidadeVal = new EntidadeValidator();
+	private FaixaValidator faixaVal = new FaixaValidator();
 
     public boolean validate(Professor professor) {
 		if(!professor.getFiliado().getCpf().isEmpty() &&
@@ -14,11 +15,14 @@ public class ProfessorValidator implements Validator<Professor> {
             professor.getFiliado().getDataNascimento() != null &&
 			!professor.getFiliado().getEmail().isEmpty() &&
 			!professor.getFiliado().getNome().isEmpty() &&
+			!professor.getFiliado().getRegistroCbj().isEmpty() &&
 			(!professor.getFiliado().getTelefone1().isEmpty() || !professor.getFiliado().getTelefone2().isEmpty()) &&
 			rgVal.validate(professor.getFiliado().getRg()) &&
 			enderecoVal.validate(professor.getFiliado().getEndereco()) &&
 			professor.getEntidades().size() > 0 &&
-			entidadeVal.validate_list(professor.getEntidades()))
+			entidadeVal.validate_list(professor.getEntidades()) &&
+			professor.getFiliado().getFaixas().size() > 0 &&
+			faixaVal.validate_list(professor.getFiliado().getFaixas()))
 		{
 			return true;
 		}
